41f6bb896a7043d2fef09728774c9cc75839b635
SungJin Kim <sungjinkimm@gmail.com>
2020-04-15 22:04:16 -0400

    working buttons linked to api

--- script.js
+++ script.js
@@ -1,83 +1,85 @@
-var searchInput = document.querySelector("#searchText");
-var searchForm = document.querySelector("#searchForm");
-var searchList = document.querySelector("#searchList");
-var searches = [];
 
-init();
+var allCities = [];
 
-function renderSearch(){
+function getCityInfo(cityName){
 
-  searchList.innerHTML = "";
+  var APIkey = "b328ccab8d372c776afbedb2b4434e8c"
+  var oneDayURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIkey;
+  
+  $.ajax({
+    url: oneDayURL,
+    method: "GET"
+  }).then(function(response){
+    console.log(oneDayURL);
+    console.log(response);
+    var city = response.name;
+    var country = response.sys.country;
+    var tempK = response.main.temp;
+    var tempF = (parseFloat(tempK) - 273.15)*1.80+32;
+    var humidity = response.main.humidity;
+    var wind = response.wind.speed;
+    //var uv = response.
 
+    $("#city").text(city + ", " + country);
+    $("#temp").text(tempF + " degress Farenheit");
+    $("#humidity").text(humidity + "%");
+    $("#wind").text(wind + "MPH");
+  })
+}
 
-  for(var i=0; i<searches.length;i++){
-    var search = searches[i];
-    var li = document.createElement("li");
-    var searchAsButtons = document.createElement("button");
-    searchAsButtons.textContent = search
-    li.appendChild(searchAsButtons)
-    //li.textContent = search;
-    li.setAttribute("data-index", i);
-    searchList.append(li)
+$("#searchList").on("click", "button", function(){
+  console.log($(this).text());
+  var cityName =$(this).text();
+  getCityInfo(cityName);
+})
+
+function renderButtons(){
+  $("#searchList").html("");
+  for(i=0;i<allCities.length;i++){
+    var btn = $("<button>");
+    btn.text(allCities[i]);
+    $("#searchList").append(btn)
   }
 }
 
-function init(){
-  var storedSearch = JSON.parse(localStorage.getItem("cities"));
 
-  if(storedSearch !==null){
-    searches = storedSearch;
-  }
-  renderSearch()
-}
+$("#searchCity").on("click", function(event){
 
-function storeSearch (){
-  localStorage.setItem("cities", JSON.stringify(searches));
-}
-
-searchForm.addEventListener("submit", function(event){
   event.preventDefault();
 
-  var searchText = searchInput.value.trim();
+  var cityName = $("#cityInput").val();
+  allCities.push(cityName);
+  renderButtons();
+  
 
-  if( searchText === ""){
-    return;
-  }
-  searches.push(searchText);
-  searchInput.value = "";
+  var APIkey = "b328ccab8d372c776afbedb2b4434e8c"
+  var oneDayURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIkey;
+  
+  $.ajax({
+    url: oneDayURL,
+    method: "GET"
+  }).then(function(response){
+    console.log(oneDayURL);
+    console.log(response);
+    var city = response.name;
+    var country = response.sys.country;
+    var tempK = response.main.temp;
+    var tempF = (parseFloat(tempK) - 273.15)*1.80+32;
+    var humidity = response.main.humidity;
+    var wind = response.wind.speed;
+    //var uv = response.
 
-  storeSearch();
-  renderSearch();
+    $("#city").text(city + ", " + country);
+    $("#temp").text(tempF + " degress Farenheit");
+    $("#humidity").text(humidity + "%");
+    $("#wind").text(wind + "MPH");
+
+    
+  })
 })
 
 
 
-// WEATHER STUFF 
-// var cityName = search;
-var APIkey = "b328ccab8d372c776afbedb2b4434e8c"
-var oneDayURL = "https://api.openweathermap.org/data/2.5/weather?q=Seoul&appid=" + APIkey;
-//var oneDayURL="https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid" + APIkey;
-
-
-$.ajax({
-  url: oneDayURL,
-  method: "GET"
-}).then(function(response){
-  console.log(oneDayURL);
-  console.log(response);
-
-var city = response.name;
-var country = response.sys.country;
-var tempK = response.main.temp;
-var tempF = (parseFloat(tempK) - 273.15)*1.80+32;
-var humidity = response.main.humidity;
-var wind = response.wind.speed;
-//var uv = response.
-
-$("#city").text(city + ", " + country);
-$("#temp").text(tempF + " degress Farenheit");
-$("#humidity").text(humidity + "%");
-$("#wind").text(wind + "MPH");
 
 
 
@@ -85,4 +87,114 @@ $("#wind").text(wind + "MPH");
 
 
 
-})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var searchInput = document.querySelector("#searchText");
+// var searchForm = document.querySelector("#searchForm");
+// var searchList = document.querySelector("#searchList");
+// var searches = [];
+
+// init();
+
+// function renderSearch(){
+
+//   searchList.innerHTML = "";
+
+
+//   for(var i=0; i<searches.length;i++){
+//     var search = searches[i];
+//     var li = document.createElement("li");
+//     var searchAsButtons = document.createElement("button");
+//     searchAsButtons.textContent = search
+//     li.appendChild(searchAsButtons)
+//     //li.textContent = search;
+//     li.setAttribute("data-index", i);
+//     searchList.append(li)
+//   }
+// }
+
+// function init(){
+//   var storedSearch = JSON.parse(localStorage.getItem("cities"));
+
+//   if(storedSearch !==null){
+//     searches = storedSearch;
+//   }
+//   renderSearch()
+// }
+
+// function storeSearch (){
+//   localStorage.setItem("cities", JSON.stringify(searches));
+// }
+
+// searchForm.addEventListener("submit", function(event){
+//   event.preventDefault();
+
+//   var searchText = searchInput.value.trim();
+
+//   if( searchText === ""){
+//     return;
+//   }
+//   searches.push(searchText);
+//   searchInput.value = "";
+
+//   storeSearch();
+//   renderSearch();
+//   oneDayWeather();
+// })
+
+
+
+// // WEATHER STUFF 
+// // var cityName = search;
+
+// function oneDayWeather(){
+
+//   var cityName = searches
+//   var APIkey = "b328ccab8d372c776afbedb2b4434e8c"
+//   var oneDayURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIkey;
+//   // var oneDayURL = "https://api.openweathermap.org/data/2.5/weather?q=Seoul&appid=" + APIkey;
+  
+  
+//   $.ajax({
+//     url: oneDayURL,
+//     method: "GET"
+//   }).then(function(response){
+//     console.log(oneDayURL);
+//     console.log(response);
+  
+//   var city = response.name;
+//   var country = response.sys.country;
+//   var tempK = response.main.temp;
+//   var tempF = (parseFloat(tempK) - 273.15)*1.80+32;
+//   var humidity = response.main.humidity;
+//   var wind = response.wind.speed;
+//   //var uv = response.
+  
+//   $("#city").text(city + ", " + country);
+//   $("#temp").text(tempF + " degress Farenheit");
+//   $("#humidity").text(humidity + "%");
+//   $("#wind").text(wind + "MPH");
+//   })
+
+
+// }
+
+
+
+
+
+
