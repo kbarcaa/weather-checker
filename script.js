0244f5cedd9deaf3bae3f45bc7d495c4cbfd9019
SungJin Kim <sungjinkimm@gmail.com>
2020-04-16 22:05:05 -0400

    icons working

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
+// declaring vairables
 var allCities = [];
-
 var dateTag = new Date();
 var month = dateTag.getUTCMonth() + 1;
 var day = dateTag.getUTCDate();
 var year = dateTag.getUTCFullYear();
-
 var displayDate = month + "/" + day + "/" + year;
-
-
 
 
 // function to call on information
@@ -21,6 +18,7 @@ function getCityInfo(cityName){
     method: "GET"
   }).then(function(response){
 
+    console.log(response)
     var city = response.name;
     var country = response.sys.country;
     var tempK = response.main.temp;
@@ -28,13 +26,16 @@ function getCityInfo(cityName){
     tempF = tempF.toFixed(2);
     var humidity = response.main.humidity;
     var wind = response.wind.speed;
+    var icon = response.weather[0].icon
     
+    $("#iconHere").attr("src", "http://openweathermap.org/img/wn/"+icon+"@2x.png");
+    $("#iconHere").append(icon);
     $("#city").text(city + ", " + country + " (" + displayDate + ")");
     $("#temp").text(tempF + " °F");
     $("#humidity").text(humidity + "%");
     $("#wind").text(wind + " MPH");
 
-    // creating a nesting ajax to call upon independent UV API.
+    // creating a nesting ajax to call upon independent UV API
     var uvURL =  "https://api.openweathermap.org/data/2.5/uvi?appid=" + APIkey + "&lat=" + response.coord.lat + "&lon=" + response.coord.lon;
     $.ajax({
       url: uvURL,
@@ -91,7 +92,10 @@ $("#searchCity").on("click", function(event){
     tempF = tempF.toFixed(2)
     var humidity = response.main.humidity;
     var wind = response.wind.speed;
-  
+    var icon = response.weather[0].icon
+    
+    $("#iconHere").attr("src", "http://openweathermap.org/img/wn/"+icon+"@2x.png");
+    $("#iconHere").append(icon);
     $("#city").text(city + ", " + country + " (" + displayDate + ")");
     $("#temp").text(tempF + " °F");
     $("#humidity").text(humidity + "%");
@@ -116,7 +120,6 @@ $("#searchCity").on("click", function(event){
       }
     })
   })
-
 
   var fiveDayURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + APIkey;
   
@@ -161,7 +164,6 @@ function renderFiveDays(response2){
 
   $("#fiveDays").text("");
 
-
   for(i=0; i<fiveDayArr.length; i++){
 
     var divTag = $("<div>")
@@ -178,13 +180,15 @@ function renderFiveDays(response2){
 
     var hum = $("<h5>")
     hum.text("Humidity: " + fiveDayArr[i].main.humidity + "%")
+
+    var icon = $("<img>")
+    icon.attr("src", "http://openweathermap.org/img/wn/"+fiveDayArr[i].weather[0].icon+"@2x.png"); 
     
     divTag.append(date)
     divTag.append(temp)
     divTag.append(hum)
-    
+    divTag.append(icon)
     $("#fiveDays").append(divTag)
-
   }
 };
 //function to create an array of 5 to use in other functions
@@ -221,162 +225,5 @@ renderButtons();
 function storeSearch(){
   localStorage.setItem("cities", JSON.stringify(allCities));
 }
-
-
-
-
-
-
-///////////////////////////////////
-
-// function getFiveDay(){
-
-//   var cityName = $("#cityInput").val();
-
-//   var APIkey = "b328ccab8d372c776afbedb2b4434e8c"
-//   var fiveDayURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + APIkey;
-  
-//   $.ajax({
-//     url: fiveDayURL,
-//     method: "GET"
-//   }).then(function(response2){
-//     console.log(fiveDayURL);
-//     console.log(response2);
-//     console.log(getForecastForEachDay(response2.list))
-
-//     var fiveDayArr = getForecastForEachDay(response2.list);
-    
-
-//     // $("#monday").text(response2.list[0].main.temp)
-//     // $("#tuesday").text(response2.list[1].main.temp)
-//     // $("#wednesday").text(response2.list[2].main.temp)
-//     // $("#thursday").text(response2.list[3].main.temp)
-//     // $("#friday").text(response2.list[4].main.temp)
-
-//   })
-// }
-
-// $("#fiveDay").text("");
-
-    // for (i=0; i<fiveDayArr.length; i++){
-
-    //   var div = $("<div>");
-    //   var date = $("<h3>");
-    //   date.text("Date: " + fiveDayArr[i].dt_txt.split(" ")[0])
-
-    //   var tempK = response2.main.temp;
-    //   var tempF = (parseFloat(tempK) - 273.15)*1.80+32;
-    //   tempF = tempF.toFixed(2)
-    //   tempF.text("Temp: " + tempF)
-
-    //   div.append(date).append(tempF)
-    //   $("#fiveDay").append(div)
-
-    // }
-
-
-/*
-  The 5-Day Forecast actually breaks down each day's forecast into hour blocks of 3, which will be 
-  really hard to parse. The function below will solve the problem: 
-    1) Create a global variable called forecastArray
-    2) After you make your 5-Day Forecast API call, call this function and pass in the list of weather objects
-        (response.list)
-    3. Once this function is done, your global forecast array will be populated.
-    4. Then loop through that array to create the 5 blue blocks at the bottom
-    5. You're welcome.  :)
-  */ 
-
-// var searchInput = document.querySelector("#searchText");
-// var searchForm = document.querySelector("#searchForm");
-// var searchList = document.querySelector("#searchList");
-// var searches = [];
-
-// init();
-
-// function renderSearch(){
-
-//   searchList.innerHTML = "";
-
-
-//   for(var i=0; i<searches.length;i++){
-//     var search = searches[i];
-//     var li = document.createElement("li");
-//     var searchAsButtons = document.createElement("button");
-//     searchAsButtons.textContent = search
-//     li.appendChild(searchAsButtons)
-//     //li.textContent = search;
-//     li.setAttribute("data-index", i);
-//     searchList.append(li)
-//   }
-// }
-
-// function init(){
-//   var storedSearch = JSON.parse(localStorage.getItem("cities"));
-
-//   if(storedSearch !==null){
-//     searches = storedSearch;
-//   }
-//   renderSearch()
-// }
-
-// function storeSearch (){
-//   localStorage.setItem("cities", JSON.stringify(searches));
-// }
-
-// searchForm.addEventListener("submit", function(event){
-//   event.preventDefault();
-
-//   var searchText = searchInput.value.trim();
-
-//   if( searchText === ""){
-//     return;
-//   }
-//   searches.push(searchText);
-//   searchInput.value = "";
-
-//   storeSearch();
-//   renderSearch();
-//   oneDayWeather();
-// })
-
-
-// // WEATHER STUFF 
-// // var cityName = search;
-
-// function oneDayWeather(){
-
-//   var cityName = searches
-//   var APIkey = "b328ccab8d372c776afbedb2b4434e8c"
-//   var oneDayURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIkey;
-//   // var oneDayURL = "https://api.openweathermap.org/data/2.5/weather?q=Seoul&appid=" + APIkey;
-  
-  
-//   $.ajax({
-//     url: oneDayURL,
-//     method: "GET"
-//   }).then(function(response){
-//     console.log(oneDayURL);
-//     console.log(response);
-  
-//   var city = response.name;
-//   var country = response.sys.country;
-//   var tempK = response.main.temp;
-//   var tempF = (parseFloat(tempK) - 273.15)*1.80+32;
-//   var humidity = response.main.humidity;
-//   var wind = response.wind.speed;
-//   //var uv = response.
-  
-//   $("#city").text(city + ", " + country);
-//   $("#temp").text(tempF + " degress Farenheit");
-//   $("#humidity").text(humidity + "%");
-//   $("#wind").text(wind + "MPH");
-//   })
-
-
-// }
-
-
-
-
 
 
